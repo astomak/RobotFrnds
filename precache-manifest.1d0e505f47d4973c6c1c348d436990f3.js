b56654953b68fd26be095349c994a1548ea52379
astomak <harshakaastomak@gmail.com>
2020-07-11 16:35:28 +0530

    Updates

---
+++ precache-manifest.1d0e505f47d4973c6c1c348d436990f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d6f1dd5f05d807e9d700b53fb23f5aa",
+    "url": "/astomak/RoboFriends/index.html"
+  },
+  {
+    "revision": "0c8a36def71dbae6a96d",
+    "url": "/astomak/RoboFriends/static/css/main.55ef09f2.chunk.css"
+  },
+  {
+    "revision": "e13aa5aa07d8cdbcc425",
+    "url": "/astomak/RoboFriends/static/js/2.5e776654.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/astomak/RoboFriends/static/js/2.5e776654.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c8a36def71dbae6a96d",
+    "url": "/astomak/RoboFriends/static/js/main.089cd6f9.chunk.js"
+  },
+  {
+    "revision": "0b6a959ee41087a61f09",
+    "url": "/astomak/RoboFriends/static/js/runtime-main.3b3e4451.js"
+  },
+  {
+    "revision": "92c626406db54b5b85382bc5d5c2d88c",
+    "url": "/astomak/RoboFriends/static/media/InhumanBB_ital.92c62640.ttf"
+  }
+]);
